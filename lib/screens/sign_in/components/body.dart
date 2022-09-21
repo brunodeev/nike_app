a8b9d2ae8188c3502d482dd3006e78617e6fec0e
Bruno César <bcgmeireles@gmail.com>
2022-09-20 22:43:00 -0300

    complementando formulário / login

--- lib/screens/sign_in/components/body.dart
+++ lib/screens/sign_in/components/body.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:nike_app/components/default_button.dart';
 import 'package:nike_app/constants.dart';
 import 'package:nike_app/size_config.dart';
 
 import '../../../components/custom_surfix_icon.dart';
 import '../../../components/form_error.dart';
+import 'sign_form.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
@@ -31,7 +33,24 @@ class Body extends StatelessWidget {
                 'Entre com seu email e senha\nou continue com sua rede social',
                 textAlign: TextAlign.center,
               ),
-              SignForm(),
+              const SignForm(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SocialCard(
+                    icon: 'assets/icons/google.png',
+                    press: () {},
+                  ),
+                  SocialCard(
+                    icon: 'assets/icons/facebook.png',
+                    press: () {},
+                  ),
+                  SocialCard(
+                    icon: 'assets/icons/twitter.png',
+                    press: () {},
+                  ),
+                ],
+              ),
             ],
           ),
         ),
@@ -40,88 +59,32 @@ class Body extends StatelessWidget {
   }
 }
 
-class SignForm extends StatefulWidget {
-  const SignForm({super.key});
+class SocialCard extends StatelessWidget {
+  const SocialCard({
+    Key? key,
+    required this.icon,
+    required this.press,
+  }) : super(key: key);
 
-  @override
-  State<SignForm> createState() => _SignFormState();
-}
+  final String icon;
+  final void Function()? press;
 
-class _SignFormState extends State<SignForm> {
-  final _formKey = GlobalKey<FormState>();
-  final List<String> errors = ['Demo Error'];
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(children: [
-        buildEmailFormField(),
-        SizedBox(height: getProportionateScreenHeight(20)),
-        buildPasswordFormField(),
-        SizedBox(height: getProportionateScreenHeight(20)),
-        FormError(errors: errors),
-        DefaultButton(
-          text: 'Entrar',
-          press: () {
-            if (_formKey.currentState!.validate()) {
-              _formKey.currentState!.save();
-            }
-          },
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin:
+            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(10)),
+        padding: EdgeInsets.all(getProportionateScreenWidth(12)),
+        height: getProportionateScreenHeight(40),
+        width: getProportionateScreenWidth(40),
+        decoration: const BoxDecoration(
+          color: Color(0xFFF5F6F9),
+          shape: BoxShape.circle,
         ),
-      ]),
-    );
-  }
-
-  TextFormField buildPasswordFormField() {
-    return TextFormField(
-      obscureText: true,
-      keyboardType: TextInputType.emailAddress,
-      decoration: const InputDecoration(
-          labelText: 'Senha',
-          hintText: 'Sua senha',
-          floatingLabelBehavior: FloatingLabelBehavior.always,
-          suffixIcon: CustomSurffixIcon(
-            icon: 'assets/icons/lock.png',
-          )),
-    );
-  }
-
-  TextFormField buildEmailFormField() {
-    return TextFormField(
-      keyboardType: TextInputType.emailAddress,
-      onChanged: (value) {
-        if (value.isNotEmpty && errors.contains(kEmailNullError)) {
-          setState(() {
-            errors.remove(kEmailNullError);
-          });
-        } else if (emailValidatorRegExp.hasMatch(value) &&
-            errors.contains(kInvalidEmailError)) {
-          setState(() {
-            errors.remove(kInvalidEmailError);
-          });
-        }
-        return;
-      },
-      validator: (value) {
-        if (value!.isEmpty && !errors.contains(kEmailNullError)) {
-          setState(() {
-            errors.add(kEmailNullError);
-          });
-        } else if (!emailValidatorRegExp.hasMatch(value) &&
-            !errors.contains(kInvalidEmailError)) {
-          setState(() {
-            errors.add(kInvalidEmailError);
-          });
-        }
-        return null;
-      },
-      decoration: const InputDecoration(
-          labelText: 'Email',
-          hintText: 'Seu email',
-          floatingLabelBehavior: FloatingLabelBehavior.always,
-          suffixIcon: CustomSurffixIcon(
-            icon: 'assets/icons/email.png',
-          )),
+        child: Image.asset(icon),
+      ),
     );
   }
 }
